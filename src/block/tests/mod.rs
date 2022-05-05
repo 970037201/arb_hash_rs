c170b9fbaee5e70b7a994a1a83be7914d1fd7746
970037201 <ericpetersen3.14@gmail.com>
2022-05-05 20:33:42 +0100

    from block test

--- src/block/tests/mod.rs
+++ src/block/tests/mod.rs
@@ -13,3 +13,11 @@ fn from_slice_test() {
     const EXPECTED: [u8; 5] = [1, 2, 3, 4, 5];
     assert_eq!(NEW_BLOCK.data, EXPECTED);
 }
+
+#[test]
+fn from_block_test() {
+    const SLICE: &[u8; 7] = &[0, 6, 1, 5, 2, 4, 3];
+    const BLOCK: AHBlock<7> = AHBlock::from_slice(SLICE);
+    const NEW_BLOCK: AHBlock<7> = AHBlock::from_block(&BLOCK);
+    assert_eq!(NEW_BLOCK, BLOCK);
+}

2d1915b7968545b14f26ab557a89ff9eaf890291
970037201 <ericpetersen3.14@gmail.com>
2022-05-05 21:37:48 +0100

    Bugfix

--- src/digest/parallel_digest/tests/mod.rs
+++ src/digest/parallel_digest/tests/mod.rs
@@ -1,16 +1,15 @@
 use crate::block::AHBlock;
 
-const RND: u64 = 4;
-const LEN: usize = 6;
-const EXPECTED: AHBlock<LEN> = AHBlock::from_slice(&[0x26, 0xBE, 0xAB, 0x55, 0x01, 0x17]);
-const INPUT: [u8; 17] = [
-    0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00,
-];
-
 #[test]
 fn parallel_digest_test() {
     const THREADS: usize = 3;
+    const RND: u64 = 4;
+    const LEN: usize = 6;
+    const EXPECTED: AHBlock<LEN> = AHBlock::from_slice(&[0x26, 0xBE, 0xAB, 0x55, 0x01, 0x17]);
+    const INPUT: [u8; 17] = [
+        0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00,
+    ];
     let output = AHBlock::arb_digest_parallel::<RND>(&INPUT, THREADS);
     assert_eq!(output, EXPECTED);
 }
@@ -18,7 +17,9 @@ fn parallel_digest_test() {
 #[test]
 fn empty_digest_test() {
     const THREADS: usize = 2;
+    const RND: u64 = 3;
     const INPUT: [u8; 0] = [];
-    let output = AHBlock::arb_digest_parallel::<RND>(&INPUT, THREADS);
-    assert_eq!(output, EXPECTED);
+    const EXPECTED: [u8; 7] = [84, 116, 49, 0, 37, 201, 27];
+    let output = AHBlock::<7>::arb_digest_parallel::<RND>(&INPUT, THREADS);
+    assert_eq!(output.data, EXPECTED);
 }

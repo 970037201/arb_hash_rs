fa5590543a756138d2f97822caba3406ad05ffb6
970037201 <ericpetersen3.14@gmail.com>
2022-05-06 07:12:48 +0100

    optimization

--- src/hash/mod.rs
+++ src/hash/mod.rs
@@ -9,14 +9,27 @@ impl<const LEN: usize> AHBlock<LEN> {
     pub fn arb_hash_assign<const RND: u64>(&mut self) {
         *self = self.arb_hash::<RND>();
     }
+    
+    /// Creates a table of indexes offset into an array, wrapping.
+    #[inline(always)]
+    const fn index_table(offset: usize) -> [usize; LEN] {
+        let mut table = [0usize; LEN];
+        let mut i = 0;
+        while i < LEN {
+            table[i] = (offset + i) % LEN;
+            i += 1;
+        }
+        table
+    }
+
+    const B_IND_TABLE: [usize; LEN] = AHBlock::index_table(1);
+    const C_IND_TABLE: [usize; LEN] = AHBlock::index_table(2);
 
     /// Function for returning the hash of some block
     #[inline(always)]
     pub const fn arb_hash<const RND: u64>(&self) -> Self {
         let mut output = Self::from_block(self);
         const SHIFTS: [u32; 5] = [1, 2, 3, 5, 7];
-        let b_ind_table = index_table::<LEN, 1>();
-        let c_ind_table = index_table::<LEN, 2>();
         let mut _rnd = 0;
         while _rnd < RND {
             let mut b_shift = 0;
@@ -24,8 +37,8 @@ impl<const LEN: usize> AHBlock<LEN> {
                 let mut elem = 0;
                 while elem < LEN {
                     let shift = SHIFTS[(b_shift + elem) % 5];
-                    let b_index = b_ind_table[elem];
-                    let c_index = c_ind_table[elem];
+                    let b_index = Self::B_IND_TABLE[elem];
+                    let c_index = Self::C_IND_TABLE[elem];
                     output.data[elem] = output.data[elem].wrapping_add(output.data[b_index]);
                     output.data[c_index] ^= output.data[elem];
                     output.data[c_index] = output.data[c_index].rotate_left(shift);
@@ -37,16 +50,4 @@ impl<const LEN: usize> AHBlock<LEN> {
         }
         output.xor_block(self)
     }
-}
-
-/// Creates a table of indexes offset into an array, wrapping.
-#[inline(always)]
-const fn index_table<const LEN: usize, const OFF: usize>() -> [usize; LEN] {
-    let mut table = [0usize; LEN];
-    let mut i = 0;
-    while i < LEN {
-        table[i] = (OFF + i) % LEN;
-        i += 1;
-    }
-    table
 }
